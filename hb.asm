a02b7b53f46d4d6793427b23105e53aa3b3dac93
jasonZ <101031278+JUStkingjk@users.noreply.github.com>
2023-04-24 00:36:30 +0000

    commit

--- hb.asm
+++ hb.asm
@@ -1,1 +1,27 @@
+DATA SEGMENT
+    BUF DB 3 DUP(0);缓存接受到的小写字母
+DATA ENDS
+CODE SEGMENT
+ASSUME CS:CODE,DS:DATA
+BAGIN:
+	MOV AX,DATA
+    MOV DS,AX ;初始化数据段寄存器
 
+	MOV BX,OFFSET BUF ;bx指向缓存的首地址
+
+    MOV AH,1
+    INT 21H ;调用中断接收键盘输入（单个字符，存在AL中）
+	MOV [BX+1],AL ;放入缓存
+    
+    CMP AL,'a'
+    JNB Handler 
+	CMP AL,'z'
+	JNA Handler
+Handler1:
+
+    MOV AH,4CH
+    INT 21H
+
+	
+CODE ENDS
+END BAGIN
