903dc09637ac907330d67a65bdd19527f365685c
jasonZ <101031278+JUStkingjk@users.noreply.github.com>
2023-04-24 01:29:15 +0000

    commit

--- hb.asm
+++ hb.asm
@@ -11,14 +11,37 @@ BAGIN:
 
     MOV AH,1
     INT 21H ;调用中断接收键盘输入（单个字符，存在AL中）
-	MOV [BX+1],AL ;放入缓存
+    INC BX
+	MOV [BX],AL ;放入缓存
     
-    CMP AL,'a'
-    JNB Handler 
-	CMP AL,'z'
-	JNA Handler
-Handler1:
+    CMP [BX],'a'
+    JNB Handler1 ;判断输入大于a
+	JMP EXIT
 
+Handler1:
+	CMP [BX],'z'
+    JNA Handler2 ;判断输入小于z
+    JMP EXIT
+
+Handler2:
+	CMP [BX],'a'
+    JNE HandlerZ
+	;当输入为a时
+    MOV [BX-1],'z'
+    INC AL
+    MOV [BX+1],AL
+
+HandlerZ:  ;当输入为z时
+	CMP [BX],'z'
+    JNE Common
+	MOV [BX+1],'a'
+    DEC AL
+    MOV [BX-1],AL
+    
+Common:
+	MOV 	
+
+EXIT:
     MOV AH,4CH
     INT 21H
 
